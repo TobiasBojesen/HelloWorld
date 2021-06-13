1194483a8168d0482c51cb9aad5ce5955b3947eb
TobiasBojesen <tobiasbojesen@hotmail.dk>
2021-06-13 20:26:11 +0200

    //

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -9,7 +9,7 @@ public class HelloworldApplication {
  
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
-        //
+        ///
     }
 
 }
